8f667956946389aeb8447a29f6a9528cdb3c59e4
gulfa <gunnarroe@gmail.com>
2018-10-25 15:01:55 +0300

    Error handling for secrets

--- etl/secrets.py
+++ etl/secrets.py
@@ -7,9 +7,26 @@ def get_secret(name: str, source: str="envrionment") -> str:
         secret = os.environ.get(name, None)
     elif source == "ssm":
         client = boto3.client("secretsmanager")
-        secret = client.get_secret_value(
-            SecretId=name
+        try:
+            get_secret_value_response = client.get_secret_value(
+                SecretId=name
             )
+        except boto3.ClientError as e:
+            if e.response['Error']['Code'] == 'DecryptionFailureException':
+                raise e
+            elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+                raise e
+            elif e.response['Error']['Code'] == 'InvalidParameterException':
+                raise e
+            elif e.response['Error']['Code'] == 'InvalidRequestException':
+                raise e
+            elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+                raise e
+            else:
+                if 'SecretString' in get_secret_value_response:
+                    secret = get_secret_value_response['SecretString']
+                else:
+                    secret = base64.b64decode(get_secret_value_response['SecretBinary'])
     else:
         raise NotImplementedError(f"{source} is not a supported source")
     return secret
