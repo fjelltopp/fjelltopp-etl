879405365d6f5fae5fb63490388ae415ad495ff1
Tomasz Sabała <sabala.tomek@gmail.com>
2018-12-03 22:11:39 +0100

    Simplified aws secret manager error handling. Fixed bug for env secrets.

--- etl/secrets.py
+++ etl/secrets.py
@@ -1,13 +1,19 @@
+import base64
+
 import boto3
 import os
 import json
 from botocore.exceptions import ClientError
 
 
+class SecretsError(Exception):
+    pass
+
+
 def get_secret(name: str, source: str="envrionment") -> dict:
     if source == "environment":
-        value = os.environ.get(name, None)
-        if value is None:
+        secret = os.environ.get(name, None)
+        if secret is None:
             raise ValueError("No such secret in the envrionment")
     elif source == "ssm":
         client = boto3.client("secretsmanager")
@@ -16,16 +22,7 @@ def get_secret(name: str, source: str="envrionment") -> dict:
                 SecretId=name
             )
         except ClientError as e:
-            if e.response['Error']['Code'] == 'DecryptionFailureException':
-                raise e
-            elif e.response['Error']['Code'] == 'InternalServiceErrorException':
-                raise e
-            elif e.response['Error']['Code'] == 'InvalidParameterException':
-                raise e
-            elif e.response['Error']['Code'] == 'InvalidRequestException':
-                raise e
-            elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-                raise e
+            raise SecretsError(f"Failed to get secret form SecretManager with error code {e.response.get('Error',{}).get('Code')}")
         if 'SecretString' in get_secret_value_response:
             secret = json.loads(get_secret_value_response['SecretString'])
         else:
