1c5c430f4a03c33d065b380ea40890f685315ca6
Tomasz Sabała <sabala.tomek@gmail.com>
2018-12-12 22:15:31 +0100

    Extract get nested odk submissions to specific Ukraine implementation.

--- etl/sources.py
+++ etl/sources.py
@@ -1,7 +1,6 @@
 import requests
 import pandas as pd
 import xmltodict
-from pandas.io.json import json_normalize
 
 
 class OdkError(Exception):
@@ -9,38 +8,11 @@ class OdkError(Exception):
 
 
 def get_odk_data(aggregate_url: str, username: str, password: str, form_id: str) -> pd.DataFrame:
-    submissions = __get_odk_submissions(aggregate_url, form_id, password, username)
+    submissions = get_odk_submissions(aggregate_url, form_id, password, username)
     return pd.DataFrame(submissions)
 
-def get_flattened_odk_data(aggregate_url: str, username: str, password: str, form_id: str, deep_nested_column: str, to_split_column: str, sep='_') -> pd.DataFrame:
-    result = pd.DataFrame()
-    activity_counter = 0
-    for submission in __get_odk_submissions(aggregate_url, form_id, password, username):
-        activity_group_ = submission[deep_nested_column]
-        if type(activity_group_) != list:
-            submission[deep_nested_column] = [activity_group_]
 
-        activities_df = json_normalize(submission[deep_nested_column], sep=sep, errors='ignore')
-        activities_df_prop_split = pd.DataFrame(columns=activities_df.columns)
-        for _, activity in activities_df.iterrows():
-            activity_counter += 1
-            activity_id = f"activity_{activity_counter}"
-            for settlement in activity[to_split_column].split():
-                new_row = activity.copy()
-                new_row[to_split_column] = settlement
-                new_row['activity_id'] = activity_id
-                activities_df_prop_split = activities_df_prop_split.append(new_row)
-        activities_df_prop_split.reset_index(drop=True, inplace=True)
-        size = len(activities_df_prop_split)
-        data_frame = json_normalize(submission, sep=sep).drop(columns=[deep_nested_column])
-        data_frame_ext = pd.concat([data_frame] * size, ignore_index=True)
-
-        submission_df = pd.concat([activities_df_prop_split, data_frame_ext], axis=1)
-        result = pd.concat([result, submission_df])
-    return result
-
-
-def __get_odk_submissions(aggregate_url, form_id, password, username):
+def get_odk_submissions(aggregate_url, form_id, password, username):
     auth = requests.auth.HTTPDigestAuth(username, password)
     submissions = requests.get(aggregate_url + "/view/submissionList",
                                params={"formId": form_id}, auth=auth)
