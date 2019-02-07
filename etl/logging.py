fb5ea0e0ad8b79f18f36810e81d9bad2ddc52a1b
Tomasz Sabała <sabala.tomek@gmail.com>
2019-02-07 08:29:06 +0100

    Updated logging format.

--- etl/logging.py
+++ etl/logging.py
@@ -1,10 +1,12 @@
 import watchtower
 import logging
+LOGGING_FORMAT = '%(asctime)s - %(levelname)-7s - %(message)s'
+
 
 def get_logger(log_name: str = __name__,
                level: int=logging.INFO,
                log_group: str="etl") -> logging.Logger:
-    logging.basicConfig(level=level)
+    logging.basicConfig(level=level, format=LOGGING_FORMAT)
     logger = logging.getLogger(log_name)
     logger.addHandler(watchtower.CloudWatchLogHandler(log_group=log_group))
     return logger
