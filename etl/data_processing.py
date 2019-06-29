1a8c26d7ab2c1577826cc8b01a020788b5bd3de3
Tomasz Sabała <sabala.tomek@gmail.com>
2019-06-29 14:05:31 +0200

    Added grouper for fixed-length chunks iteration.

---
+++ etl/data_processing.py
@@ -0,0 +1,7 @@
+from itertools import izip_longest
+
+def grouper(iterable, n, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return izip_longest(*args, fillvalue=fillvalue)
