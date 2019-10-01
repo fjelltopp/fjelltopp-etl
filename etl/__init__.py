8b8b43ec22133a273914b2ec31fb71f74c7cbc02
Tomasz Sabała <sabala.tomek@gmail.com>
2019-10-01 12:14:15 +0200

    helper function to add empty df columns.

--- etl/__init__.py
+++ etl/__init__.py
@@ -4,3 +4,9 @@ LOGGER = None
 
 def null_step(df:pd.DataFrame) -> pd.DataFrame:
     return df
+
+def add_empty_column(column_name):
+    def _add_empty_column(df:pd.DataFrame) -> pd.DataFrame:
+        df[column_name] = ''
+        return df
+    return _add_empty_column
