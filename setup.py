2d22700728ac1417b887efdcec16e47c4fe28f6e
Tomasz Sabała <tomek@fjelltopp.org>
2022-01-28 09:30:39 +0100

    Version 0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fjelltopp-etl',
-    version='0.0.6',
+    version='0.0.7',
     long_description=__doc__,
     packages=['etl'],
     include_package_data=True,
